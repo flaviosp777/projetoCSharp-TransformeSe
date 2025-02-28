818efde7919fdfd4489b3eeeb26ac90afcfaed0e
flaviosp777 <flavio.jcjesus@senacsp.edu.br>
2025-02-28 17:21:20 -0300

    Janela de menu principal

--- LojasABC/frmLogin.cs
+++ LojasABC/frmLogin.cs
@@ -16,5 +16,25 @@ namespace LojasABC
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSair_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
     }
 }
--- LojasABC/frmSplash.cs
+++ LojasABC/frmSplash.cs
@@ -16,5 +16,15 @@ namespace LojasABC
         {
             InitializeComponent();
         }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void frmSplash_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
